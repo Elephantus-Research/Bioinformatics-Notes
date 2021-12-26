d93c77c66d9f306c65947cc65b3c2a08f941041c
roleyder <roggergarciadiaz@gmail.com>
2021-12-25 18:40:29 -0600

    Greedy Algorithm

--- StringMotifs.py
+++ StringMotifs.py
@@ -13,7 +13,7 @@ def Count(Motifs):
 
 def Profile(Motifs):
     t = len(Motifs)
-    k = len(Motif[0])
+    k = len(Motifs[0])
     profile = Count(Motifs)
     for k,v in profile.items():
         v[:] = [x/t for x in v]
@@ -41,5 +41,60 @@ def Score(Motifs):
                     count += 1
     return count
 
+def PrCompute(Motif, profile):
+    c = 1
+    for i in range(len(Motif)-1):
+        for nucleotide in profile:
+            if Motif[i]==nucleotide:
+                c*=profile[nucleotide][i]
+    return c
 
-print(Score("TTCCGG"))
+def Pr(Text, Profile):
+    p = 1
+    for i in range(len(Text)):
+        p = p*Profile[Text[i]][i]
+    return p
+
+def PrMostProbableKmer(text,k,profile):
+    pr = {}
+    mpk =[]
+    for i in range(len(text)-k+1):
+        kmer=text[i:i+k]
+        pro = Pr(kmer, profile)
+        pr[kmer] = pro
+    m = max(pr.values())
+    for k,v in pr.items():
+        if pr[k] == m:
+            mpk.append(k)
+    return mpk[0]
+
+def GreedyMotifSearch(Dna,k,t):
+    best_motifs = []
+    for i in range(0,t):
+        best_motifs.append(Dna[i][0:k])
+    n = len(Dna[0])
+    for m in range(n-k+1):
+        Motifs=[]
+        Motifs.append(Dna[i][0:k])
+        for j in range(i,t):
+            p = Profile(Motifs[0:j])
+            Motifs.append(PrMostProbableKmer(Dna[j],k,p))
+        if Score(Motifs) < Score(best_motifs):
+            best_motifs = Motifs
+    return best_motifs
+
+Dna = [
+"GGCGTTCAGGCA",
+"AAGAATCAGTCA",
+"CAAGGAGTTCGC",
+"CACGTCAATCAC",
+"CAATAATATTCG"
+]
+Motif = "TCGTGGATTTCC" 
+profile = {
+    'A': [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.9, 0.1, 0.1, 0.1, 0.3, 0.0],
+    'C': [0.1, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.1, 0.2, 0.4, 0.6],
+    'G': [0.0, 0.0, 1.0, 1.0, 0.9, 0.9, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
+    'T': [0.7, 0.2, 0.0, 0.0, 0.1, 0.1, 0.0, 0.5, 0.8, 0.7, 0.3, 0.4]
+}
+print(GreedyMotifSearch(Dna, 3, 5))
